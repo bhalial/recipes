53bafc0842b57b4fb3f780eb4b133ea2b3b56cd0
bhalial <jstengs@gmail.com>
2023-03-27 22:53:22 +0200

    spacing reduced recipe pages
    spacing list items reduced in recipe pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -186,11 +186,11 @@ module.exports = {
             listStyleType: 'decimal',
           },
           'ul, ol': {
-            paddingLeft: theme('spacing.6'),
+            paddingLeft: theme('spacing.2'),
           },
           li: {
-            marginTop: theme('spacing.6'),
-            marginBottom: theme('spacing.6'),
+            marginTop: theme('spacing.0'),
+            marginBottom: theme('spacing.0'),
             paddingLeft: theme('spacing[3.5]'),
           },
           'li::marker': {
